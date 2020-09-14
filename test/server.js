9214cdda366f725f424d0f1fbea762d188f31512
joshuar84 <1984yearoftherat@gmail.com>
2020-09-14 11:17:09 -1000

    finished phase 2b

--- test/server.js
+++ test/server.js
@@ -5,10 +5,42 @@ http.createServer(function(req, res){
         console.log("Inbound 'OK' being processed");
         res.writeHead(200)
         res.end();
-    } else {
+    } else if (req.url === '/404') {
         console.log("Error code 404")
         res.writeHead(404)
         res.end()
+    } else if (req.url === '/Created') {
+        console.log('Inbound created');
+        res.writeHead(201);
+        res.end();
+    } else if (req.url === '/Moved-Permanently') {
+        console.log('Resource moved');
+        res.writeHead(301);
+        res.end();
+    } else if (req.url === '/Found') {
+        console.log('Resource moved temporarily');
+        res.writeHead(302);
+        res.end();
+    } else if (req.url === '/Bad-Request') {
+        console.log('Couldn\'t understand request');
+        res.writeHead(400);
+        res.end();
+    } else if (req.url === '/Unauthorized') {
+        console.log('Not proper authentication');
+        res.writeHead(401);
+        res.end();
+    } else if (req.url === '/Forbidden') {
+        console.log('Improper credentials');
+        res.writeHead(403);
+        res.end();
+    } else if (req.url === '/Internal-Server-Error') {
+        console.log('Server error');
+        res.writeHead(500);
+        res.end();
+    } else if (req.url === '/Gateway-Timeout') {
+        console.log('Slow server response');
+        res.writeHead(504);
+        res.end();
     }
     res.end();
 }).listen(3000, function(){
