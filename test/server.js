c4b13e411850ec130922304fabc9300eaf706ec7
joshuar84 <1984yearoftherat@gmail.com>
2020-09-14 12:21:47 -1000

    some bonus problems

--- test/server.js
+++ test/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 http.createServer(function(req, res){
     if (req.url === "/OK") {
         console.log("Inbound 'OK' being processed");
-        res.writeHead(200)
+        res.writeHead(200);
+        res.write('<h1>Hello world</h1>')
         res.end();
     } else if (req.url === '/Created') {
         console.log('Inbound created');
@@ -11,11 +12,11 @@ http.createServer(function(req, res){
         res.end();
     } else if (req.url === '/Moved-Permanently') {
         console.log('Resource moved');
-        res.writeHead(301, {Google: "http://www.google.com"});
+        res.writeHead(301, {'Location': "http://www.google.com"});
         res.end();
     } else if (req.url === '/Found') {
         console.log('Resource moved temporarily');
-        res.writeHead(302);
+        res.writeHead(302, {'Location': 'https://www.github.com'});
         res.end();
     } else if (req.url === '/Bad-Request') {
         console.log('Couldn\'t understand request');
